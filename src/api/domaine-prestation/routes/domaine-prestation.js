b23a554f1d8e2476095eecac34a5300aba0a9bd6
Bouchbout-sami <ks_bouchbout@esi.dz>
2024-06-21 03:34:56 +0100

    deploy try 1

---
+++ src/api/domaine-prestation/routes/domaine-prestation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * domaine-prestation router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::domaine-prestation.domaine-prestation');
---
+++ src/api/domaine-prestation/services/domaine-prestation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * domaine-prestation service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::domaine-prestation.domaine-prestation');
